b00434d6643f4d111f8ac788437fc86c58b2a90e
Nithin RS <nithin007raj@gmail.com>
2024-12-21 21:18:28 +0530

    initial commit

---
+++ lib/db/db_helper.dart
@@ -0,0 +1,103 @@
+import 'package:path/path.dart' as P;
+import 'package:sqflite/sqflite.dart';
+import 'package:vcard_project/models/contact_model.dart';
+class DbHelper {
+  final String _createTableContact = '''create table $tableContact(
+  $tblContactColId integer primary key autoincrement, 
+  $tblContactColName text, 
+  $tblContactColMobile text, 
+  $tblContactColEmail text, 
+  $tblContactColAddress text, 
+  $tblContactColCompany text, 
+  $tblContactColDesignation text, 
+  $tblContactColWebsite text, 
+  $tblContactColImage text, 
+  $tblContactColFavorite integer)''';
+
+  //
+  Future<Database> _open() async {
+    final root = await getDatabasesPath();
+    final dbPath = P.join(root, 'contact.db');
+    return openDatabase(dbPath, version: 2, onCreate: (db, version) {
+     db.execute(_createTableContact);
+    }, onUpgrade: (db, oldVersion, newVersion) async {
+      if(oldVersion == 1)  {
+        await db.execute('alter table $tableContact rename to ${'contact_old'}');
+        await db.execute(_createTableContact);
+        final rows = await db.query('contact_old');
+        for(var row in rows) {
+          await db.insert(tableContact, row);
+        }
+        await db.execute('drop table if exists ${'contact_old'}');
+      }
+     });
+  }
+  // Future<Database> _open() async {
+  //   final root = await getDatabasesPath();
+  //   final dbPath = P.join(root, 'contact.db');
+  //   return openDatabase(
+  //     dbPath,
+  //     version: 2,
+  //     onCreate: (db, version) {
+  //    //   print('Creating  table: $tableContact');
+  //       db.execute(_createTableContact);
+  //     },
+  //     onUpgrade: (db, oldVersion, newVersion) async {
+  //       if (oldVersion == 1) {
+  //         // Check if the table exists before renaming
+  //         List<Map> result = await db.rawQuery(
+  //             "SELECT name FROM sqlite_master WHERE type='table' AND name='$tableContact';");
+  //         if (result.isNotEmpty) {
+  //           await db.execute('ALTER TABLE $tableContact RENAME TO contact_old;');
+  //           await db.execute(_createTableContact);
+  //           final rows = await db.query('contact_old');
+  //           for (var row in rows) {
+  //             await db.insert(tableContact, row);
+  //           }
+  //           await db.execute('DROP TABLE IF EXISTS contact_old;');
+  //         } else {
+  //           await db.execute(_createTableContact);
+  //         }
+  //       }
+  //     },
+  //   );
+  // }
+
+  Future<int> insertContact(ContactModel contactModel) async {
+    final db = await _open();
+    return db.insert(tableContact, contactModel.toMap());
+  }
+
+  Future<List<ContactModel>> getAllContacts() async {
+    final db = await _open();
+    final mapList = await db.query(tableContact);
+    return List.generate(mapList.length, (index) => ContactModel.fromMap(mapList[index]));
+  }
+
+  Future<ContactModel> getContactById(int id) async {
+    final db = await _open();
+    final mapList = await db.query(tableContact, where: ' $tblContactColId = ?', whereArgs: [id]);
+    return ContactModel.fromMap(mapList.first);
+  }
+
+  Future<List<ContactModel>> getAllFavoriteContacts() async {
+    final db = await _open();
+    final mapList = await db.query(tableContact, where: ' $tblContactColFavorite = ?', whereArgs: [1]);
+    return List.generate(mapList.length, (index) => ContactModel.fromMap(mapList[index]));
+  }
+
+  Future<int> deleteContact(int id) async {
+    final db = await _open();
+    return db.delete(tableContact, where: ' $tblContactColId = ?', whereArgs: [id]);
+  }
+
+  // Future<int> updateContactField(int id, Map<String, dynamic> map) async {
+  //   final db = await _open();
+  //   return db.update(tableContact, map, where: ' $tblContactColId = ?', whereArgs: [id]);
+  // }
+
+  Future<int> updateFavorite(int id, int value) async {
+    final db = await _open();
+    return db.update(tableContact, {tblContactColFavorite: value}, where: ' $tblContactColId = ?', whereArgs: [id]);
+  }
+}
---
+++ lib/utils/constants.dart
@@ -0,0 +1,12 @@
+abstract final class ContactProperties {
+  static const String name = "Name";
+  static const String mobile = "Mobile";
+  static const String email = "Email";
+  static const String address = "Address";
+  static const String company = "Company";
+  static const String designation = "Designation";
+  static const String website = "Website";
+}
+
+const String emptyFieldErrMsg = 'This field must not be empty';
+const String hint = 'Long press and drag each item from the below list and drop above in the appropriate box. You can drop multiple items over a single box.';
